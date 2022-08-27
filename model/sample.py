bde67a5e84e47e1051349d6390f4d9a33123774c
taneemishere <taneemishere@gmail.com>
2022-08-27 06:53:28 +0500

    code refactoring

--- model/sample.py
+++ model/sample.py
@@ -29,8 +29,7 @@ meta_dataset = np.load("{}/meta_dataset.npy".format(trained_weights_path), allow
 input_shape = meta_dataset[0]
 output_size = meta_dataset[1]
 
-#changed the pix2code original model to our main-model
-model = main-model(input_shape, output_size, trained_weights_path)
+model = Main_Model(input_shape, output_size, trained_weights_path)
 model.load(trained_model_name)
 
 sampler = Sampler(trained_weights_path, input_shape, output_size, CONTEXT_LENGTH)
